cb43d7f8b584710e2ab8a523637e08cc619d0d7d
gabi emi <gabiemi2021@gmail.com>
2025-10-25 18:18:42 -0300

    Minha página: modo escuro

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const botao = document.getElementById("botao");
+const body = document.body;
+
+botao.addEventListener("click", () => {
+    if (body.classList.contains("claro")) {
+        body.classList.replace("claro", "escuro")
+        botao.textContent = "Ativar modo claro ☀️"
+    } else {
+        body.classList.replace("escuro", "claro")
+        botao.textContent = "Ativar modo escuro 🌙"
+    }
+});
